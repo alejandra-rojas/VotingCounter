fd8a9b4141e4e39ea90f13f5ac0d70aa633ec4f5
Alejandra Rojas <alejandra.rojas@hyperisland.se>
2022-11-17 10:22:27 +0100

    local storage and databases deploy

--- scripts.js
+++ scripts.js
@@ -1,60 +1,55 @@
 'use strict';
 
-const firebaseConfig = {
+const config = {
     apiKey: "AIzaSyBFd4ZQpffzs-i9TIkm7Y4VXNojTEk1WPw",
     authDomain: "votecounter-c33c4.firebaseapp.com",
-    projectId: "votecounter-c33c4",
+    databaseURL: "https://votecounter-c33c4-default-rtdb.firebaseio.com",
     storageBucket: "votecounter-c33c4.appspot.com",
-    messagingSenderId: "1035469983693",
-    appId: "1:1035469983693:web:6b9fe647c7a1072c45be84",
-    measurementId: "G-6ZDH6DT3HE"
-  };
-  
-  // Initialize Firebase
-  const app = firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
-
-
-
-const subEl1 = document.getElementById('submission1');
-const subEl2 = document.getElementById('submission2');
-const add1 = document.getElementById('single');
-const add1a = document.getElementById('singleA');
-
-// state that we need to store over the course of our program
-let el1Votes = 0;
-let el2Votes = 0;
-
-
-// HOME FUNCTIONS
-// function that can be used in the two events to update value.
-// removing the step of having to update html
-function updateCounter (value) {
-    el1Votes = value
-    subEl1.innerHTML = el1Votes;
-    if (el1Votes > el2Votes){
-        subEl1.style.backgroundColor = 'black';
-    } 
+    messagingSenderId: "1035469983693"
 };
 
-// event listeners on functions
-add1.onclick = function (){
-    updateCounter(el1Votes + 1);
-};
+firebase.initializeApp(config);
 
-// AWAY FUNCTIONS
-  function updateCounterA (value) {
-    el2Votes = value
-    subEl2.innerHTML = el2Votes;
-    if (el1Votes < el2Votes){
-        subEl1.style.backgroundColor = 'black';
-    } 
-};
-
-// event listeners on functions
-add1a.onclick = function (){
-    updateCounterA(el2Votes + 1);
-};
+const dCounters = document.querySelectorAll('.CountLike');
+const userHasVoted = localStorage.getItem('iHaveVoted', 'yes');
 
 
+
+[].forEach.call(dCounters, function(dCounter) {
+    const el = dCounter.querySelector('button');
+    const cId = dCounter.id;
+    const dDatabase = firebase.database().ref('VotesCounter').child(cId);
+    
+    // get firebase data
+    dDatabase.on('value', function(snap) {
+        let data = snap.val() || 0;
+        dCounter.querySelector('span').innerHTML = data;
+    });
+    
+    // set firebase data
+    el.addEventListener('click', function() {
+          if (userHasVoted) {
+            let message = document.createElement('div');
+            message.innerHTML = "You've already voted.";
+            message.className = "redalert";
+            document.body.append(message);
+            setTimeout ( function () {
+                message.classList.add("invisible");
+                }, 1800);  
+
+         } else {
+             dDatabase.transaction(function(dCount) {
+                return (dCount || 0) + 1;
+            });
+            el.style.color = 'red'
+            localStorage.setItem('iHaveVoted', 'yes');
+            window.location.reload()
+            let div = document.createElement('div');
+            div.className = "alert";
+            div.innerHTML = "Thank you for voting!";
+            document.body.append(div);
+         }
+        
+    });
+});
 
